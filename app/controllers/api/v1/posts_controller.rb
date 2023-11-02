f4117d6270d03bba0cc3f8a6ece31fed487b0086
EC2 Default User <ec2-user@ip-10-0-0-216.ap-northeast-1.compute.internal>
2023-11-02 14:30:18 +0000

    index

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,19 +1,33 @@
 class Api::V1::PostsController < ApplicationController
     include JwtAuthenticator
-    before_action ->(request) { authenticate_request(request) }, only: [:create, :update, :destroy]
+    before_action ->(request) { authenticate_request(request) }, only: [:index,:create, :update, :destroy]
   
   
   def index
-    page = params[:page].to_i || 1
+    user_id = authenticate_request(request) # Assign the value returned by authenticate_request to user_id
+      p"================"
+      p user_id
+      p"================"
+    if user_id.nil?
+      render json: { status: 401, error: "Unauthorized" }
+      return
+    end
+    token = encode(user_id) # Use the correct user_id here
+    user = User.find_by(id: user_id)
+    if user.nil?
+      render json: { status: 404, error: "User not found" }
+      return
+    end
+    page = params[:page].present? ? params[:page].to_i : 1
     per_page = 100
 
     offset = (page - 1) * per_page
     posts = Post.order(created_at: :desc).offset(offset).limit(per_page)
 
     post_data = []
-        posts.each do |post|
-          post_data << post.as_json(except: [:created_at, :updated_at])
-        end
+    posts.each do |post|
+      post_data << post.as_json(except: [:created_at, :updated_at])
+    end
       render json: post_data
   end
   
